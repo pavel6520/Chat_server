4f33876ef5083b877e8c67a9e16254c7e5faf3dc
Павел Музыченко <pavmuz2012@yandex.ru>
2019-05-10 13:16:04 +0300

    bootstrap navbar с логином юзера

--- headerLayout/headerLayout.cs
+++ headerLayout/headerLayout.cs
@@ -6,11 +6,48 @@ using System.Text;
 using System.Threading.Tasks;
 
 public class headerLayout : LayoutWorker {
+	public headerLayout() {
+		staticInclude = new string[] { "auth" };
+	}
+
 	public void Init() {
-		Echo("<header  class=\"page-header\">");
-		if (_helper.Auth != null) {
-			Echo($"<h3>{_helper.Auth.Login}</h3>");
+		if (!_helper.isAuth) {
+			PluginWorker auth = (PluginWorker)_helper.staticPlugins["auth"];
+			bool res = (bool)auth._Work(_helper, "checkSession");
+			_helper = auth._GetHelper();
 		}
-		Echo("</header>");
+		Echo("<header class=\"navbar navbar-expand-md navbar-light bg-light fixed-top\" style=\"z-index: 100;\">" +
+			"<a class=\"navbar-brand\" href=\"/\">Chat by pavel6520</a>" +
+   "<button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">" +
+   "<span class=\"navbar-toggler-icon\"></span>" +
+   "</button>" +
+   "<div class=\"collapse navbar-collapse\" id=\"navbarContent\">" +
+   "<ul class=\"navbar-nav mr-auto\">" +
+   "<li class=\"nav-item dropdown\">" +
+   "<a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\"" +
+   $">{(_helper.isAuth && _helper.Auth.Status ? _helper.Auth.Login : "Account")}</a>" +
+   "<div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">");
+		if (_helper.isAuth && _helper.Auth.Status) {
+			Echo("<a class=\"dropdown-item\" href=\"#\">Action</a>" +
+			"<a class=\"dropdown-item\" href=\"#\">Another action</a>" +
+			"<div class=\"dropdown-divider\"></div>" +
+			"<a class=\"dropdown-item\" href=\"#\">Something else here</a>");
+		}
+		else {
+			Echo("<a class=\"dropdown-item\" href=\"/auth/login\">Log-in</a>" +
+			"<a class=\"dropdown-item\" href=\"/auth/signin\">Sign-in</a>");
+		}
+   Echo("</div>" +
+   "</li>" +
+   "<li class=\"nav-item\">" +
+   "<a class=\"nav-link disabled\" href=\"#\">Disabled</a>" +
+   "</li>" +
+   "</ul>" +
+   "<form class=\"form-inline my-2 my-lg-0\">" +
+   "<input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\">" +
+   "<button class=\"btn btn-outline-success my-2 my-sm-0\" type=\"submit\">Search</button>" +
+   "</form>" +
+   "</div>" +
+   "</header>");
 	}
 }
