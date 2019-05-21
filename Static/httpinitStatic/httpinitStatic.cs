c4e77631fbcedeaba12a06d791952bf708adf5f6
Павел Музыченко <pavmuz2012@yandex.ru>
2019-05-21 10:47:54 +0300

    инициализация http

--- Static/httpinitStatic/httpinitStatic.cs
+++ Static/httpinitStatic/httpinitStatic.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 
 public class httpinitStatic : PluginWorker {
 	public httpinitStatic() {
+		staticInclude = new string[] { "auth" };
+	}
 
+	public void Init() {
+		var auth = _helper.GetPlugin("auth");
+		auth._Work(_helper, "checkSession");
+		_helper = auth._GetHelper();
 	}
 }
