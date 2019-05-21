36ef794163fb406d8f285b99d24ef1ee9df37648
Павел Музыченко <pavmuz2012@yandex.ru>
2019-05-21 11:23:03 +0300

    WebSocketInit

---
+++ Static/websocketinitStatic/websocketinitStatic.cs
@@ -0,0 +1,18 @@
+﻿using ConnectionWorker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class websocketinitStatic : PluginWorker {
+	public websocketinitStatic() {
+		staticInclude = new string[] { "auth" };
+	}
+
+	public void Init() {
+		var auth = _helper.GetPlugin("auth");
+		auth._Work(_helper, "checkSession");
+		_helper = auth._GetHelper();
+	}
+}
